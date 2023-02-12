abf35c581289109ce1e257b480e73616f5a2a810
alexberg90 <ayazya@code.edu.az>
2023-02-12 13:00:17 +0400

    js intro

---
+++ assets/tasks/js/index.js
@@ -0,0 +1,18 @@
+let num=4235482125
+
+if (num%21==0) {
+    console.log("3 ve 7-ye bolunur");
+
+}else 
+console.log("bolunmur");
+
+
+
+let m=8;
+let n=8;
+
+if (m%2==0 && n%2==0) {
+    console.log(n+m);
+} else {
+    console.log("ededlerden biri ve ya her ikisi tekdir");
+}
